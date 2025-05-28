86909f4522e7f34ddf9bbf36ebd06912db7f7374
ras23244 <rashmiiii203@gmail.com>
2025-05-28 04:30:28 -0700

    user backend register login done

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
@@ -17,7 +17,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        minlength: 6
+        minlength: 6,
+        select: false
     },
     role: {
         type: String,
@@ -25,3 +26,21 @@ const userSchema = new mongoose.Schema({
         default: 'user'
     }
 }, { timestamps: true });
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    return token;
+}
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+}
+
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user',userSchema);
+
+module.exports = userModel;
